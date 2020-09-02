6170482d544bdbfd13655afa576d8830cd1260dc
EC2 Default User <ec2-user@ip-172-31-30-59.us-east-2.compute.internal>
2020-09-02 23:49:03 +0000

    changed the root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello' # This line sets the 'root' route to the 'application' controller found in the controllers folder and runs the action 'hello' defined inside there!
-  
+  # root 'application#hello' # This line sets the 'root' route to the 'application' controller found in the controllers folder and runs the action 'hello' defined inside there!
+  root 'application#goodbye'
 end
