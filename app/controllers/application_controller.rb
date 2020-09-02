3f5de6f32e337ee1ac91b084feb2759f4a1d3921
EC2 Default User <ec2-user@ip-172-31-30-59.us-east-2.compute.internal>
2020-09-02 23:06:21 +0000

    Added the hello-world action in the controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # After recieving a request from the browser, the controller will interact with a model/db to render a view for the user!
+  def hello
+    render html: "hello world!"
+  end
+  
 end
